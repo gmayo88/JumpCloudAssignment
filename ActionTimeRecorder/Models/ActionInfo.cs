dd0c427bc7705b3222f25847f9c5afd8627f1b9e
Gabriel Mayo <gmayo88@gmail.com>
2021-07-26 20:38:52 -0500

    Added ActionInfo Model and Json dependency

---
+++ ActionTimeRecorder/Models/ActionInfo.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ActionTimeRecorder.Models
+{
+    class ActionInfo
+    {
+        [JsonProperty("action", Required = Required.Always)]
+        public string Action { get; set; }
+
+        [JsonProperty("time", Required = Required.Always)]
+        public int Time { get; set; }
+    }
+}
