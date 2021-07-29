a21b5b8ee92ef3d5c1e8406ec6416cd1871fd136
Gabriel Mayo <gmayo88@gmail.com>
2021-07-28 19:49:28 -0500

    Added ActionStats model

---
+++ ActionTimeRecorder/Models/ActionStats.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ActionTimeRecorder.Models
+{
+    class ActionStats
+    {
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonProperty("avg")]
+        public double Avg { get; set; }
+    }
+}
